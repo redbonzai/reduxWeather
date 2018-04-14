4361749523d1a606373527fdad1205bea8bc2bb1
Christian Dominguez <christian@redbonzai.com>
2018-04-14 14:52:02 -0500

    first commit reduxWeather

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+const API_KEY = 'de235288a5f2a9fd5d865f75d2bec841';
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+export const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export function fetchWeather(city) {
+
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
---
+++ src/containers/weather-list.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class WeatherList extends Component {
+    render() {
+        return (
+            <table className="table table-hover">
+                <thead>
+                    <tr>
+                        <th>City</th>
+                        <th>Temperature</th>
+                        <th>Pressure</th>
+                        <th>Humidity</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                </tbody>
+            </table>
+        );
+
+    }
+
+}
+
+function mapStateToProps(state) {
+    return { weather: state.weather };
+}
+
+//above method could also be in es-6:
+/*function mapStateToProps({ weather }) {
+    return { weather };
+}*/
+
+export default connect( mapStateToProps)(WeatherList);
